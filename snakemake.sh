838a6b6e4dfb838f0a8fa57826a1b3d1712edc24
Skyler Kuhn <kuhnsa2@mymail.vcu.edu>
2018-03-27 16:43:45 -0400

    Update snakemake.sh

--- snakemake.sh
+++ snakemake.sh
@@ -2,12 +2,12 @@
 # This file is is used to submit the snakemake job
 # Usage: sbatch --cpus-per-task=32 --mem=64g snakemake.sh
 
-module load snakemake samtools peakranger macs gem music PePr R || exit 1
+module load snakemake samtools peakranger macs gem music PePr bedtools sicer R || exit 1
 
 sbcmd="sbatch --cpus-per-task={threads} --mem={cluster.mem}"
-sbcmd+=" --time={cluster.time} --partition={cluster.partition}"
+sbcmd+=" --time={cluster.time} --job-name={cluster.jobname} --partition={cluster.partition}"
 sbcmd+=" --out={cluster.out} {cluster.extra}"
 
-snakemake -pr --keep-going --local-cores $SLURM_CPUS_PER_TASK \
+snakemake -pr --keep-going --rerun-incomplete --local-cores $SLURM_CPUS_PER_TASK \
     --jobs --cluster-config cluster.json --cluster "$sbcmd" \
     --latency-wait 180 all
